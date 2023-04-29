7693a0a323102eec2e1746dc73dc79ef9ee585bb
mikos.pawel <mikos.pawel@o2.pl>
2023-04-29 22:04:20 +0200

    Generyczny stos int string double

---
+++ AdvRentalApp/AdvRentalApp/BasicStack.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace AdvRentalApp
+{
+    internal class BasicStack<T>
+    {
+        private readonly T[] _items;
+
+        private int _curentIndex = 0;
+        public BasicStack() => _items = new T[10];
+        public int Count => _curentIndex    ;
+
+        public void Push(T item) => _items[_curentIndex++] = item;
+        public T Pop() => _items[--_curentIndex];
+    }
+}
--- AdvRentalApp/AdvRentalApp/Program.cs
+++ AdvRentalApp/AdvRentalApp/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdvRentalApp;
+using System.IO.Pipes;
+
+var stack = new BasicStack<double>();
+var stackString = new BasicStack<string>();
+var stackInt =new BasicStack<int>();
+
+stack.Push(item:4.5);
+stack.Push(item: 43);
+stack.Push(item: 333.6);
+stackInt.Push(item: 8);
+stackInt.Push(item: 53);
+stackInt.Push(item: 633);
+stackString.Push(item: "company1");
+stackString.Push(item: "company2");
+stackString.Push(item: "company3");
+double sum = 0.0;
+
+while(stack.Count > 0)
+{
+    double item =stack.Pop();
+    Console.WriteLine(item);
+    sum += item;
+}
+Console.WriteLine($"sumDouble: {sum}\n");
+sum = 0;
+while (stackInt.Count > 0)
+{
+    int item = stackInt.Pop();
+    Console.WriteLine(item);
+    sum += item;
+}
+Console.WriteLine($"sumInt: {sum}\n");
+while (stackString.Count > 0)
+{
+    string item = stackString.Pop();
+    Console.WriteLine(item);   
+}
